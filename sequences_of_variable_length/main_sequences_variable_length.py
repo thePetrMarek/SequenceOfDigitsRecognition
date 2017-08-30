39337d46eabc823e91a822be78f4ec01155800ab
Petr Marek <ermrk.marek@gmail.com>
2017-08-30 18:54:59 +0200

    Deeper model of sequence of variable lengths

--- sequences_of_variable_length/main_sequences_variable_length.py
+++ sequences_of_variable_length/main_sequences_variable_length.py
@@ -4,9 +4,10 @@ import numpy as np
 import tensorflow as tf
 
 from localization_dataset import LocalizationDataset
-from sequences_of_variable_length.deep_localization_weighted_loss_variable_length import \
-    DeepLocalizationWeightedLossVariableLength
+from sequences_of_variable_length.deep_localization_weighted_loss_variable_length_deeper import \
+    DeepLocalizationWeightedLossVariableLengthDeeper
 from visualize import Visualize
+import matplotlib.pyplot as plt
 
 
 def get_batch(dataset, inputs_placeholder, labels_placeholder, positions_placeholder, keep_prob_placeholder,
@@ -21,9 +22,10 @@ def get_batch(dataset, inputs_placeholder, labels_placeholder, positions_placeho
                       keep_prob_placeholder: keep_prob_val,
                       is_training_placeholder: is_traininig}, "end_of_file": batch["end_of_file"]}
 
+
 def evaluate(dataset, session, operation, inputs_placeholder, labels_placeholder, positions_placeholder,
              keep_prob_placeholder,
-             is_training_placeholder,model_name, name,
+             is_training_placeholder, model_name, name,
              summary_writer, learning_step, visualize_correct=0, visualize_incorrect=0):
     visualize = Visualize()
     correct_visualized_counter = 0
@@ -57,7 +59,8 @@ def evaluate(dataset, session, operation, inputs_placeholder, labels_placeholder
                     visualize.visualize_with_correct_label_position(batch[inputs_placeholder][i], predictions[i],
                                                                     true_label,
                                                                     predicted_positions[i],
-                                                                    batch[positions_placeholder][i], os.path.join(model_name, name) + "_correct")
+                                                                    batch[positions_placeholder][i],
+                                                                    os.path.join(model_name, name) + "_correct")
                     correct_visualized_counter += 1
                 elif incorrect_visualized_counter < visualize_incorrect and corrects_vector[i] == False:
                     visualize.visualize_with_correct_label_position(batch[inputs_placeholder][i], predictions[i],
@@ -72,9 +75,9 @@ def evaluate(dataset, session, operation, inputs_placeholder, labels_placeholder
     position_error = total_position_error / (number_of_examples / 50)
 
     summary = tf.Summary()
-    summary.value.add(tag='Sequence_accuracy_'+name, simple_value=sequence_accuracy)
-    summary.value.add(tag='Character_accuracy_'+name, simple_value=character_accuracy)
-    summary.value.add(tag='Position_error_'+name, simple_value=position_error)
+    summary.value.add(tag='Sequence_accuracy_' + name, simple_value=sequence_accuracy)
+    summary.value.add(tag='Character_accuracy_' + name, simple_value=character_accuracy)
+    summary.value.add(tag='Position_error_' + name, simple_value=position_error)
     summary_writer.add_summary(summary, learning_step)
 
     print("Number of correct examples: " + str(correct_num) + "/" + str(number_of_examples))
@@ -86,14 +89,52 @@ def evaluate(dataset, session, operation, inputs_placeholder, labels_placeholder
     print()
 
 
-if __name__ == '__main__':
+def calculate_normalization_parameters():
+    train_localization = LocalizationDataset("../train_variable_localization.p")
+    all_training_example = False
+    sum = None
+    number_of_examples = 0
+    while not all_training_example:
+        loaded = train_localization.load(50)
+        all_training_example = loaded["end_of_file"]
+        examples = loaded["examples"]
+        for example in examples:
+            if sum is None:
+                sum = example
+            else:
+                sum += example
+        number_of_examples += len(examples)
+    mean = sum / number_of_examples
 
+    train_localization = LocalizationDataset("../train_variable_localization.p")
+    all_training_example = False
+    squares = None
+    while not all_training_example:
+        loaded = train_localization.load(50)
+        all_training_example = loaded["end_of_file"]
+        examples = loaded["examples"]
+        for example in examples:
+            if squares is None:
+                squares = np.power(example - mean, 2)
+            else:
+                squares += np.power(example - mean, 2)
+    std = np.sqrt(squares / number_of_examples)
+    plt.imshow(mean, cmap='gray')
+    plt.show()
+
+    plt.imshow(std, cmap='gray')
+    plt.show()
+    return mean, std
+
+
+if __name__ == '__main__':
     # Load dataset
     train_localization = LocalizationDataset("../train_variable_localization.p")
 
     with tf.Graph().as_default():
         # Wiring
-        model = DeepLocalizationWeightedLossVariableLength()
+        # model = DeepLocalizationWeightedLossVariableLength()
+        model = DeepLocalizationWeightedLossVariableLengthDeeper()
 
         inputs_placeholder, labels_placeholder, positions_placeholder, keep_prob_placeholder, is_training_placeholder = model.input_placeholders()
         logits, predicted_positions = model.inference(inputs_placeholder, keep_prob_placeholder,
@@ -121,7 +162,7 @@ if __name__ == '__main__':
         steps = 30000
         for step in range(steps + 1):
             batch = get_batch(train_localization, inputs_placeholder, labels_placeholder, positions_placeholder,
-                              keep_prob_placeholder, 1,
+                              keep_prob_placeholder, 0.8,
                               is_training_placeholder, True)["batch"]
             loss_value, summary, _ = session.run([loss, merged_summary, training], feed_dict=batch)
             writer.add_summary(summary, step)
